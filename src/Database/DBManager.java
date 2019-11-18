9c16197aaea88a0e236bbf3809ed817293793651
SangWooJeong <jsu4838@naver.com>
2019-11-18 14:45:49 +0900

    코드 정리 및 JavaDoc 레퍼런스 추가

--- src/Database/DBManager.java
+++ src/Database/DBManager.java
@@ -11,10 +11,18 @@ import java.util.ArrayList;
 public class DBManager {
     public static byte STATUS_NORMAL = 0, STATUS_CANCELED = 1, TYPE_SELL = 1, TYPE_DELIVERY = 2;
 
+
+    /**
+    * 로그 출력 여부
+    */
     public boolean isNoisy() {
         return noisy;
     }
 
+    /**
+     * 로그 출력 여부 설정
+     * @param noisy 여부
+     */
     public void setNoisy(boolean noisy) {
         this.noisy = noisy;
     }
@@ -30,6 +38,13 @@ public class DBManager {
         initialize();
     }
 
+    /**
+     * 일반적인 생성자
+     * @param IP IP번호
+     * @param DB DB이름
+     * @param ID 유저ID
+     * @param PW 비밀번호
+     */
     public DBManager(String IP, String DB, String ID, String PW) {
         server = IP; // MySQL 서버 주소
         database = DB; // MySQL DATABASE 이름
@@ -38,41 +53,65 @@ public class DBManager {
         initialize();
     }
 
-    public ResultSet editStock(Stock tstock) {
+    /**
+     * 품목 변경
+     * @param stock 해당 품목 데이터
+     * @return 성공 여부
+     */
+    public boolean editStock(Stock stock) {
         Connection con = getConnection();
         Statement stat = getStatement(con);
-        String msg = String.format("INSERT INTO tstock VALUES ('%s', '%s', %d) ON DUPLICATE KEY UPDATE sname='%s',sprice=%d;", tstock.getKey(), tstock.getName(), tstock.getPrice(), tstock.getName(), tstock.getPrice());
+        String msg = String.format("INSERT INTO tstock VALUES ('%s', '%s', %d) ON DUPLICATE KEY UPDATE sname='%s',sprice=%d;", stock.getKey(), stock.getName(), stock.getPrice(), stock.getName(), stock.getPrice());
         ResultSet ret = null;
+        boolean result = false;
         try {
             ret = stat.executeQuery(msg);
+            result = true;
         } catch (SQLException e) {
+            result = false;
             e.printStackTrace();
+        } finally {
+            commit(con);
+            close(stat);
+            close(con);
+            return result;
         }
-        commit(con);
-        close(stat);
-        close(con);
-        return ret;
     }
 
-    public ResultSet getStock(String key) {
+    /**
+     * 품목 리스트 요청
+     * @param key 키값
+     * @return 품목 데이터
+     */
+    public Stock getStock(String key) {
         Connection con = getConnection();
         Statement stat = getStatement(con);
         ResultSet ret = null;
+        Stock result = null;
+        String msg = "SELECT * FROM tstock where skey=%s";
         try {
-            ret = stat.executeQuery("SELECT * FROM tstock");
+            ret = stat.executeQuery(String.format(msg, key));
+            ret.first();
+            int index = 0;
+            result = new Stock(ret.getString("skey"), ret.getString("sname"), ret.getInt("sprice"));
         } catch (SQLException e) {
             e.printStackTrace();
         }
         commit(con);
         close(stat);
         close(con);
-        return ret;
+        return result;
     }
 
-    public long addEvent(Event tevent) {
+    /**
+     * 거래기록 추가
+     * @param event 추가할려는 기록(id 없이 요청)
+     * @return 추가하여 얻은 키
+     */
+    public long addEvent(Event event) {
         Connection con = getConnection();
         Statement stat = getStatement(con);
-        String msg = String.format("INSERT INTO tevent (etype,etime,estatus,ememo) VALUES (%d, '%s', %d,'%s');", tevent.getType(), tevent.getTime().toString(), tevent.getStatus(), tevent.getMemo());
+        String msg = String.format("INSERT INTO tevent (etype,etime,estatus,ememo) VALUES (%d, '%s', %d,'%s');", event.getType(), event.getTime().toString(), event.getStatus(), event.getMemo());
         ResultSet ret = null;
         try {
             stat.executeQuery(msg);
@@ -89,21 +128,26 @@ public class DBManager {
         }
 
 
-        for (Change c : tevent.getData()) {
+        for (Change c : event.getData()) {
             c.setEventKey(retKey);
             addChange(c);
         }
         commit(con);
         close(stat);
         close(con);
-        tevent.setKey(retKey);
+        event.setKey(retKey);
         return retKey;
     }
 
-    private long addChange(Change tchange) {
+    /**
+     * 변동 개별기록 추가
+     * @param change 개별기록
+     * @return 추가된 키
+     */
+    private long addChange(Change change) {
         Connection con = getConnection();
         Statement stat = getStatement(con);
-        String msg = String.format("INSERT INTO tchange (cevent,cstock,cnumber) VALUES (%d,'%s',%d);", tchange.getEventKey(), tchange.getStockKey(), tchange.getAmount());
+        String msg = String.format("INSERT INTO tchange (cevent,cstock,cnumber) VALUES (%d,'%s',%d);", change.getEventKey(), change.getStockKey(), change.getAmount());
         long retKey = 0;
         try {
             stat.executeQuery(msg);
@@ -113,12 +157,16 @@ public class DBManager {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        tchange.setKey(retKey);
+        change.setKey(retKey);
         close(stat);
         close(con);
         return retKey;
     }
 
+    /**
+     * 품목 리스트 요청
+     * @return 품목 리스트
+     */
     public Stock[] getStocks() {
         Connection con = getConnection();
         Statement stat = getStatement(con);
@@ -147,6 +195,11 @@ public class DBManager {
         return result;
     }
 
+    /**
+     * 이벤트 요청
+     * @param key 키값
+     * @return 이벤트
+     */
     public Event getEvent(long key) {
         Connection con = getConnection();
         Statement stat = getStatement(con);
@@ -176,7 +229,13 @@ public class DBManager {
         return result;
     }
 
-    public boolean tryCancelEvent(long key, byte status) {
+    /**
+     * 거래기록 상태 변경 시도
+     * @param key 기록 키
+     * @param status 변경할려는 상태
+     * @return 성공 여부
+     */
+    public boolean tryChangeEvent(long key, byte status) {
         Connection con = getConnection();
         Statement stat = getStatement(con);
         ResultSet ret = null;
@@ -196,6 +255,11 @@ public class DBManager {
         }
     }
 
+    /**
+     * 거래 기록 리스트 요청
+     * @param type 거래 기록 타입
+     * @return 해당 타입으로 된 간략한 기록
+     */
     public EventList[] getEventList(byte type) {
         Connection con = getConnection();
         Statement stat = getStatement(con);
@@ -225,10 +289,18 @@ public class DBManager {
         return result;
     }
 
+    /**
+     * Noisy여부에 따른 로그 출력
+     * @param msg 메세지
+     */
     private void log(String msg) {
         if (noisy) System.out.println(msg);
     }
 
+    /**
+     * 연결 생성
+     * @return 연결
+     */
     private Connection getConnection() {
         Connection con = null;
         // 1.드라이버 로딩
@@ -250,6 +322,11 @@ public class DBManager {
         return con;
     }
 
+    /**
+     * 구문 생성
+     * @param con 연결
+     * @return 구문
+     */
     private Statement getStatement(Connection con) {
         try {
             return con.createStatement();
@@ -259,6 +336,10 @@ public class DBManager {
         return null;
     }
 
+    /**
+     * 커밋 수행
+     * @param con 연결
+     */
     private void commit(Connection con) {
         try {
             con.commit();
@@ -267,15 +348,23 @@ public class DBManager {
         }
     }
 
-    private void close(Statement con) {
+    /**
+     * 구문 닫기
+     * @param stat 구문
+     */
+    private void close(Statement stat) {
         // 3.해제
         try {
-            if (con != null)
-                con.close();
+            if (stat != null)
+                stat.close();
         } catch (SQLException e) {
         }
     }
 
+    /**
+     * 연결 닫기
+     * @param con 연결
+     */
     private void close(Connection con) {
         // 3.해제
         try {
@@ -285,6 +374,9 @@ public class DBManager {
         }
     }
 
+    /**
+     * 초기화 : 테이블이 없으면 새로 정의
+     */
     private void initialize() {
         Connection con = getConnection();
         Statement stat = getStatement(con);
