d2c913d85bae8344a04398c47df465f7c679d147
김상훈 <tkdgnsdl556@naver.com>
2019-11-19 23:25:21 +0900

    아 졸려

--- src/Console/Main.java
+++ src/Console/Main.java
@@ -1,7 +1,40 @@
 package Console;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]){
+        System.out.println("1=판매, 2=납품 ,3=재고");
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> arraylist = new ArrayList<String>();
+
+        String to;
+
+        to = scanner.next();
+        if (to == "1"){
+            while(true){
+                System.out.println("코드 입력 or stop");
+                String input=scanner.nextLine();
+                if(input.equals("stop")) {
+                    System.out.println(arraylist);
+                    break;
+                }
+                else arraylist.add(input);
+
+            }
+
+        }
+        else if (to == "2"){
+
+
+        }
+        else if (to =="3"){
+
+        }
+        else{
+            System.out.println("장난 Nope");
+        }
 
     }
 }
