492720ccdbf415bd1ec215bf2cba89abdf1ce222
SangWooJeong <jsu4838@naver.com>
2019-11-26 16:51:58 +0900

    메인 수정

--- src/Manager/MainForDB.java
+++ src/Manager/MainForDB.java
@@ -10,19 +10,20 @@ import java.util.Random;
 
 public class MainForDB {
     public static void main(String args[]){
-        Random rand=new Random();
-        DBManager dbManager=new DBManager();
+        for(int i=0;i<10;i++) {
+            Random rand = new Random();
+            DBManager dbManager = new DBManager();
 
-        dbManager.editStock(new Stock("1","alpha",100));
-        dbManager.editStock(new Stock("2","bravo",200));
-        dbManager.editStock(new Stock("3","charlie",300));
-        dbManager.editStock(new Stock("4","delta",400));
-        dbManager.editStock(new Stock("5","echo",500));
+            dbManager.editStock(new Stock("1", "alpha", 100));
+            dbManager.editStock(new Stock("2", "bravo", 200));
+            dbManager.editStock(new Stock("3", "charlie", 300));
+            dbManager.editStock(new Stock("4", "delta", 400));
+            dbManager.editStock(new Stock("5", "echo", 500));
 
-        //TODO 품목수정
-        dbManager.editStock(new Stock("6","foxtrot",600));
+            //TODO 품목수정
+            dbManager.editStock(new Stock("6", "foxtrot", 600));
 
-        //TODO 내역 추가
+            //TODO 내역 추가
         /*
         Event event=new Event(Event.TYPE_SELL, Timestamp.valueOf(LocalDateTime.now()),"Testing");
         for(int i=0;i<6;i++){
@@ -31,15 +32,16 @@ public class MainForDB {
         }
         dbManager.addEvent(event);
         */
-        //dbManager.setNoisy(true);
-        System.out.println(dbManager.getEvent(2));
-        for(EventList t :dbManager.getEventList(DBManager.TYPE_SELL)){
-            System.out.println(t.toString());
-        }
-        System.out.println();
-        System.out.println("2019-11-15 11:27:30 ~ 2019-12-15 11:27:30 거래기록");
-        for(Stock t : dbManager.getSelling(Timestamp.valueOf("2019-11-15 11:27:30"),Timestamp.valueOf("2019-12-15 11:27:30"))){
-            System.out.println(t.toString());
+            //dbManager.setNoisy(true);
+            System.out.println(dbManager.getEvent(2));
+            for (EventList t : dbManager.getEventList(DBManager.TYPE_SELL)) {
+                System.out.println(t.toString());
+            }
+            System.out.println();
+            System.out.println("2019-11-15 11:27:30 ~ 2019-12-15 11:27:30 거래기록");
+            for (Stock t : dbManager.getSelling(Timestamp.valueOf("2019-11-15 11:27:30"), Timestamp.valueOf("2019-12-15 11:27:30"))) {
+                System.out.println(t.toString());
+            }
         }
     }
 
