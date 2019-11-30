20297c6a09d57dcb6cb16fda4b38b51d5fad75e4
An wonseok <nave0048@naver.com>
2019-11-30 17:45:45 +0900

    주석 수정

--- src/Manager/ServerController.java
+++ src/Manager/ServerController.java
@@ -80,7 +80,7 @@ public class ServerController {
 
     /**
      * 현재 "getStocks" 작동 확인
-     * NetworkManager에서 받은 소켓에서 보내는 데이터를 해석하고 DBManager를 호출한 후, ack를 반환
+     * Network Manager에서 받은 소켓에서 보내는 데이터를 해석하고 DBManager를 호출한 후, ack를 반환
      * 인수와 반환값은 String
      * []안의 값은 대치되어야 함
      * @param networkMsg "editStock" + " " + [key] + " " + [name] + " " + [price]
@@ -91,7 +91,7 @@ public class ServerController {
      * @return [key] [name] [price],[key] [name] [price] [amount] ,... : stock마다 ,로 구분된 띄워쓰기로 연결된 문자열
      * @param networkMsg "getEvent" + [eventKey]
      * @return [type] [time] [memo] [c.stockKey] [c.amount] [c.eventKey] + [c.key],... : change마다 ,로 구분된 띄워쓰기로 연결된 문자열
-     * @param networkMsg "tryChangEvent" + [eventKey] + [status] : status(0 Nomal, 1 Cancle, 2 NaN)
+     * @param networkMsg "tryChangEvent" + [eventKey] + [status] : status(0 Normal, 1 Cancel, 2 NaN)
      * @return true or false : 성공, 실패
      * @param networkMsg "getEventList" + [type] : type(1 Sell, 2 delivery, 3 NaN)
      * @return [key] [type] [totalPrice],... : event마다 ,로 구분된 띄워쓰기로 연결된 문자열
