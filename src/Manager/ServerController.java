1590f159d7dba91afdd497cb62896c7ae13b7faa
An wonseok <nave0048@naver.com>
2019-12-07 14:28:53 +0900

    주석 보강, 예외처리 추가, getSelling 오버라이딩 대응
    getSelling(Timestamp start, Timestamp end, byte type) 대응

--- src/Manager/ServerController.java
+++ src/Manager/ServerController.java
@@ -1,46 +1,86 @@
 package Manager;
 
+/*
+    change : 한 stock에 대한 변화 기록
+    Event : change들로 이루어진 하나의 사건
+    EventList : event들로 이루어진 리스트
+    stock : 재고의 키, 품명, 가격, 수량을 가지고 있는 클래스
+ */
 import Data.Change;
 import Data.Event;
 import Data.EventList;
 import Data.Stock;
+
+/*
+    데이터베이스 매니저
+    DB에 query 후 result 반환
+ */
 import Database.DBManager;
 
+// yyyy-MM-dd hh:mm:ss 형식 (year, month, day, hour, minute, second)
 import java.sql.Timestamp;
+
+// String 변수를 Timestamp로 변환하기 위해 사용
 import java.text.SimpleDateFormat;
+
+// 날짜 계산
 import java.util.Date;
+
+// cui 입력
 import java.util.Scanner;
+
+// String을 데이터 단위로 끊기 위해 사용 split()과 차이점 주의
 import java.util.StringTokenizer;
 
-
+/*
+    네트워크로 전달된 메시지를 파싱하여 디비매니저를 호출 후, 반환
+ */
 public class ServerController {
 
     private NetworkManager networkManager;
     private DBManager dbManager;
-    private Scanner scanner = new Scanner(System.in);
+    private Scanner scanner;
+    private static ServerController serverController;
+
+    private ServerController() { }
+
+    public ServerController getInstance()
+    {
+        if(serverController == null)
+            serverController = new ServerController();
+
+        return serverController;
+    }
 
     /*
     초기화
     NetworkManager 스레드 생성
      */
-    public void initialize() {
+    private void initialize() {
         new Thread(() -> {
             networkManager = new NetworkManager(this);
         }).start();
 
         dbManager = new DBManager("localhost", "androidpos", "root", "1234");
+        scanner = new Scanner(System.in);
     }
 
     public static void main(String[] args) {
-        ServerController serverController = new ServerController();
+        serverController = new ServerController();
 
         serverController.initialize();
 
         serverController.run();
     }
 
+    /*
+        종료하기 위한 cui
+        종료는 exit
+        새로운 스레드를 생성하여 입력받기를 대기하고
+        메인 스레드는 Thread.sleep문 반복하며 대기
+     */
     private void run() {
-        //종료하기 위한 cui
+        //TODO 상훈이 코드 연결
         System.out.println("Point of Sales System");
 
         for (int i = 0; i < 30; i++)
@@ -74,32 +114,35 @@ public class ServerController {
         }
     }
 
-    /**DB와 통신을 위한 메소드*/
-    /**
-     * 현재 "getStocks" 작동 확인
+    /**DB와 통신을 위한 메소드
      * Network Manager에서 받은 소켓에서 보내는 데이터를 해석하고 DBManager를 호출한 후, ack를 반환
      * 인수와 반환값은 String
      * []안의 값은 대치되어야 함
-     * @param "editStock" + " " + [key] + " " + [name] + " " + [price]
-     * @return  true or false : 성공, 실패
-     * @param "getStock" + [key]
-     * @return [key] [name] [price] : 띄워쓰기로 연결된 문자열
-     * @param "getStocks"
-     * @return [key] [name] [price],[key] [name] [price] [amount] ,... : stock마다 ,로 구분된 띄워쓰기로 연결된 문자열
-     * @param "getEvent" + " " + [eventKey]
-     * @return [type] [time] [memo] [c.stockKey] [c.amount] [c.eventKey] + [c.key],... : change마다 ,로 구분된 띄워쓰기로 연결된 문자열
-     * @param "tryChangEvent" + " " + [eventKey] + " " + [status] : status(0 Normal, 1 Cancel, 2 NaN)
-     * @return true or false : 성공, 실패
-     * @param "getEventList" + " " + [type] : type(1 Sell, 2 delivery, 3 NaN)
-     * @return [key] [time] [totalPrice],... : event마다 ,로 구분된 띄워쓰기로 연결된 문자열
-     * @param "getSelling" + " " + [startTime] + " " + [endTime] : startTime, endTime은 타임스탬프 형식(yyyy-MM-dd hh:mm:ss)
-     * @return [key] [name] [price] [amount],... : startTime과 endTime 사이에 판매된 stock마다 ,로 구분된 띄워쓰기로 연결된 문자열
-     * @param "addEvent" + " " + [type] + " " + [time] + " " + [status] + " " + [memo]
-     * @return [eventKey]
-     * @param "addChange" + " " + [eventKey] + " " + [stockKey] + " " + [changedAmount]
-     * @return [eventKey]
+     * ( A, B ) 는 A or B
+     * 인수가 포맷을 따르지 않을 경우 invalid syntax
+     * 인수안에 필요한 데이터가 없으면 해당 데이터를 요구하는 스트링 반환                                                                 ex) "input key"
+     * DBManager의 리턴이 null이면 null반환
+     * @param "editStock" + " " + [key] + " " + [name] + " " + [price]                                                              ex) "editStock 1 alpha 100"
+     * @return  (true, false) : 성공, 실패                                                                                           ex) "true"
+     * @param "getStock" + [key]                                                                                                    ex) "getStock 1"
+     * @return [key] [name] [price]                                                                                                 ex) "1 alpha 100"
+     * @param "getStocks"                                                                                                           ex) "getStocks"
+     * @return [key] [name] [price],[key] [name] [price] [amount] ,... : stock마다 ,로 구분된 띄워쓰기로 연결된 문자열                  ex) "1 alpha 100, 2 bravo 200,"
+     * @param "getEvent" + " " + [eventKey]                                                                                         ex) "getEvent 2"
+     * @return [type] [time] [memo] [c.stockKey] [c.amount] [c.eventKey] + [c.key],... : change마다 ,로 구분된 띄워쓰기로 연결된 문자열 ex) "DELIVERY 2019-12-05 14:40:31.0 delivering 1 1 2 6,2 1 2 7,3 1 2 8,4 1 2 9,"
+     * @param "tryChangEvent" + " " + [eventKey] + " " + [status] : status(0 Normal, 1 Cancel, 2 NaN)                               ex) "tryChangeEvent 1 0"
+     * @return (true, false) : 성공, 실패                                                                                            ex) "false"
+     * @param "getEventList" + " " + [type] : type(1 Sell, 2 delivery, 3 NaN)                                                       ex) "getEventList 1"
+     * @return [key] [time] [totalPrice],... : event마다 ,로 구분된 띄워쓰기로 연결된 문자열                                            ex) "2 2019-12-05 14:40:31.0 1000,"
+     * @param "getSelling" + " " + [startTime] + " " + [endTime] : startTime, endTime은 타임스탬프 형식(yyyy-MM-dd hh:mm:ss)          ex) "getSelling 2019-11-1 2019-12-17"
+     * @return [key] [name] [price] [amount],... : startTime과 endTime 사이에 판매된 stock마다 ,로 구분된 띄워쓰기로 연결된 문자열        ex) "1 alpha 100 -2,2 bravo 200 -7,3 charlie 300 -1,5 echo 500 -1,6 foxtrot 600 -1,"
+     * @param "addEvent" + " " + [type] + " " + [time] + " " + [status] + " " + [memo]                                              ex) "addEvent 2 2019-12-07 14:00:26.443 0 delivering"
+     * @return [eventKey]                                                                                                           ex) "4"
+     * @param "addChange" + " " + [eventKey] + " " + [stockKey] + " " + [changedAmount]                                             ex) "addChange 4 1 1"
+     * @return ([changeKey], false)                                                                                                 ex) "12"
      */
     public String parseAndExecuteData(String networkMsg) {
+        //null 입력 받을 시 null 반환
         if(networkMsg==null)
             return "input error";
 
@@ -107,138 +150,167 @@ public class ServerController {
         stringTokenizer = new StringTokenizer(networkMsg, " ");
         String opcode = stringTokenizer.nextToken();
 
-        if (opcode.equals("editStock")) {
-            if (!stringTokenizer.hasMoreTokens())
-                return "input key";
-            String key = stringTokenizer.nextToken();
+        try {
+            if (opcode.equals("editStock")) {
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input key";
+                String key = stringTokenizer.nextToken();
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "input name";
-            String name = stringTokenizer.nextToken();
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input name";
+                String name = stringTokenizer.nextToken();
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "input price";
-            int price = Integer.parseInt(stringTokenizer.nextToken());
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input price";
+                int price = Integer.parseInt(stringTokenizer.nextToken());
 
-            return Boolean.toString(dbManager.editStock(new Stock(key, name, price)));
-        } else if (opcode.equals("getStock")) {
-            String key = stringTokenizer.nextToken();
+                return Boolean.toString(dbManager.editStock(new Stock(key, name, price)));
+            } else if (opcode.equals("getStock")) {
+                String key = stringTokenizer.nextToken();
+                Stock stock = dbManager.getStock(key);
+                return stock != null ? dbManager.getStock(key).toString(0) : null;
+            } else if (opcode.equals("getStocks")) {
+                String ackMsg = "";
+                Stock[] stocks = dbManager.getStocks();
 
-            return dbManager.getStock(key).toString(0);
-        } else if (opcode.equals("getStocks")) {
-            String ackMsg = "";
-            Stock[] stocks = dbManager.getStocks();
-            for (Stock s : stocks) {
-                ackMsg = ackMsg + s.toString() + ",";
-            }
-            return ackMsg;
-        } else if (opcode.equals("getEvent")) {
-            if (!stringTokenizer.hasMoreTokens())
-                return "input key";
-            Long key = toLong(stringTokenizer.nextToken());
-            Event event = dbManager.getEvent(key);
-            return event != null ? event.toString(0) : "null";
-        } else if (opcode.equals("addChange")) {
-            if (!stringTokenizer.hasMoreTokens())
-                return "input eventKey";
-            String eventKey = stringTokenizer.nextToken();
-            if (!stringTokenizer.hasMoreTokens())
-                return "input stockKey";
-            String stockKey = stringTokenizer.nextToken();
-            if (!stringTokenizer.hasMoreTokens())
-                return "input changedAmount";
-            String changedAmount = stringTokenizer.nextToken();
+                if(stocks == null)
+                    return null;
 
-            Change change = new Change(stockKey, toInt(changedAmount));
-            change.setEventKey(toLong(eventKey));
+                for (Stock s : stocks) {
+                    ackMsg = ackMsg + s.toString() + ",";
+                }
 
-            return Long.toString(dbManager.addChange(change));
-        } else if (opcode.equals("tryChangeEvent")) {
-            if (!stringTokenizer.hasMoreTokens())
-                return "input key";
-            Long key = toLong(stringTokenizer.nextToken());
-            if (!stringTokenizer.hasMoreTokens())
-                return "status";
-            Byte status = toByte(stringTokenizer.nextToken());
+                return ackMsg;
+            } else if (opcode.equals("getEvent")) {
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input key";
 
-            return Boolean.toString(dbManager.tryChangeEvent(key, status));
-        } else if (opcode.equals("getEventList")) {
-            if (!stringTokenizer.hasMoreTokens())
-                return "type";
-            Byte type = toByte(stringTokenizer.nextToken());
-            EventList[] event = dbManager.getEventList(type);
-            String ackMsg = "";
+                Long key = toLong(stringTokenizer.nextToken());
+                Event event = dbManager.getEvent(key);
 
-            for (EventList e : event) {
-                ackMsg = ackMsg.concat(e.toString());
-                ackMsg = ackMsg.concat(",");
-            }
+                return event != null ? event.toString(0) : "null";
+            } else if (opcode.equals("addChange")) {
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input eventKey";
+                String eventKey = stringTokenizer.nextToken();
 
-            return ackMsg;
-        } else if (opcode.equals("getSelling")) {
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input stockKey";
+                String stockKey = stringTokenizer.nextToken();
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "input start time";
-            String startTime = stringTokenizer.nextToken() + " 00:00:00";
-            Timestamp startTimeStamp = null;
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input changedAmount";
+                String changedAmount = stringTokenizer.nextToken();
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "input end time";
-            String endTime = stringTokenizer.nextToken() + " 00:00:00";
-            Timestamp endTimeStamp = null;
+                Change change = new Change(stockKey, toInt(changedAmount));
+                change.setEventKey(toLong(eventKey));
 
-            try {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-                Date parsedDate = dateFormat.parse(startTime);
-                startTimeStamp = new java.sql.Timestamp(parsedDate.getTime());
-                parsedDate = dateFormat.parse(endTime);
-                endTimeStamp = new java.sql.Timestamp(parsedDate.getTime());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Stock[] statistickList = dbManager.getSelling(startTimeStamp, endTimeStamp);
+                Long ack = dbManager.addChange(change);
 
-            String ackMsg = "";
-            for (int i = 0; i < statistickList.length; i++) {
-                ackMsg = ackMsg.concat(statistickList[i].toString());
-                ackMsg = ackMsg.concat(",");
-            }
-            return ackMsg;
-        } else if (opcode.equals("addEvent")) {
-            if (!stringTokenizer.hasMoreTokens())
-                return "input type";
-            Byte type = toByte(stringTokenizer.nextToken());
+                if(ack == -1)
+                    return "false";
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "input time";
-            String time = stringTokenizer.nextToken();
+                return Long.toString(dbManager.addChange(change));
+            } else if (opcode.equals("tryChangeEvent")) {
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input key";
+                Long key = toLong(stringTokenizer.nextToken());
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "invalid time";
-            time = time.concat(" " + stringTokenizer.nextToken());
-            
-            Timestamp timestamp = null;
-            try {
-                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
-                Date parsedDate = dateFormat.parse(time);
-                timestamp = new java.sql.Timestamp(parsedDate.getTime());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+                if (!stringTokenizer.hasMoreTokens())
+                    return "status";
+                Byte status = toByte(stringTokenizer.nextToken());
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "input status";
-            Byte status = toByte(stringTokenizer.nextToken());
+                return Boolean.toString(dbManager.tryChangeEvent(key, status));
+            } else if (opcode.equals("getEventList")) {
+                if (!stringTokenizer.hasMoreTokens())
+                    return "type";
+                Byte type = toByte(stringTokenizer.nextToken());
 
-            if (!stringTokenizer.hasMoreTokens())
-                return "input memo";
-            String memo = stringTokenizer.nextToken();
+                EventList[] event = dbManager.getEventList(type);
+                String ackMsg = "";
 
-            Event event = new Event(type, timestamp, memo);
-            event.setStatus(status);
-            return Long.toString(dbManager.addEvent(event));
-        } else
-            return "invalid syntax";
+                for (EventList e : event) {
+                    ackMsg = ackMsg.concat(e.toString() + ",");
+                }
+
+                return ackMsg;
+            } else if (opcode.equals("getSelling")) {
+
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input start time";
+                String startTime = stringTokenizer.nextToken() + " 00:00:00";
+                Timestamp startTimeStamp = null;
+
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input end time";
+                String endTime = stringTokenizer.nextToken() + " 00:00:00";
+                Timestamp endTimeStamp = null;
+
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+                    Date parsedDate = dateFormat.parse(startTime);
+                    startTimeStamp = new java.sql.Timestamp(parsedDate.getTime());
+                    parsedDate = dateFormat.parse(endTime);
+                    endTimeStamp = new java.sql.Timestamp(parsedDate.getTime());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Stock[] statistickList;
+
+                if(stringTokenizer.hasMoreTokens())
+                {
+                    byte type = toByte(stringTokenizer.nextToken());
+                    statistickList = dbManager.getSelling(startTimeStamp, endTimeStamp, type);
+                }
+                else
+                    statistickList = dbManager.getSelling(startTimeStamp, endTimeStamp);
+
+                String ackMsg = "";
+                for (int i = 0; i < statistickList.length; i++) {
+                    ackMsg = ackMsg.concat(statistickList[i].toString());
+                    ackMsg = ackMsg.concat(",");
+                }
+                return ackMsg;
+            } else if (opcode.equals("addEvent")) {
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input type";
+                Byte type = toByte(stringTokenizer.nextToken());
+
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input time";
+                String time = stringTokenizer.nextToken();
+
+                if (!stringTokenizer.hasMoreTokens())
+                    return "invalid time";
+                time = time.concat(" " + stringTokenizer.nextToken());
+
+                Timestamp timestamp = null;
+                try {
+                    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+                    Date parsedDate = dateFormat.parse(time);
+                    timestamp = new java.sql.Timestamp(parsedDate.getTime());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input status";
+                Byte status = toByte(stringTokenizer.nextToken());
+
+                if (!stringTokenizer.hasMoreTokens())
+                    return "input memo";
+                String memo = stringTokenizer.nextToken();
+
+                Event event = new Event(type, timestamp, memo);
+                event.setStatus(status);
+                return Long.toString(dbManager.addEvent(event));
+            } else
+                return "invalid syntax";
+        } catch ( Exception e)
+        {
+            e.printStackTrace();
+            return "unexpected ack";
+        }
     }
 
     private Byte toByte(String str) {
